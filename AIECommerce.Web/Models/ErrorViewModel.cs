06aed053448f4da41459ac66628e5a4c4dcd10bd
aleynayilm <alynylmazz@gmail.com>
2025-02-26 20:04:16 +0300

    project initialized

---
+++ AIECommerce.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AIECommerce.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
